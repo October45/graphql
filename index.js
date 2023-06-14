0416bde3e19c120c1aeb2a82ac547830f5b96c96
October45 <vikto45.t@gmail.com>
2023-06-14 13:54:42 +0300

    "mer än väl"

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.getElementById('signInForm').addEventListener('submit', signIn);
+document.getElementById('logOutBtn').addEventListener('click', logOut);
 validateJWT();
 
 /**
@@ -9,6 +10,7 @@ async function validateJWT() {
     const token = getToken();
     if (!token) {
         document.getElementById('signIn').classList.remove('d-none');
+        document.getElementById('logOutBtn').disabled = true;
         return false;
     }
     var res;
@@ -31,6 +33,7 @@ async function validateJWT() {
             return !c.startsWith('token');
         });
         document.getElementById('errorMsg').classList.remove('d-none');
+        document.getElementById('logOutBtn').disabled = true;
         return false;
     }
     const data = await res.json();
@@ -39,11 +42,23 @@ async function validateJWT() {
             return !c.startsWith('token');
         });
         document.getElementById('signIn').classList.remove('d-none');
+        document.getElementById('logOutBtn').disabled = true;
         return false;
     }
     document.getElementById('signIn').classList.add('d-none');
     showInfo();
+    document.getElementById('logOutBtn').disabled = false;
     return true;
+}
+
+/**
+ * Logs the user out by removing the token cookie
+ */
+function logOut() {
+    document.cookie = 'token=';
+    document.getElementById('signIn').classList.remove('d-none');
+    document.getElementById('content').classList.add('d-none');
+    document.getElementById('logOutBtn').disabled = true;
 }
 
 /**
@@ -72,10 +87,12 @@ async function signIn(e) {
         document
             .getElementById('signInErrorMsg')
             .getElementById('msg').innerHTML = err.error;
+        document.getElementById('logOutBtn').disabled = true;
     }
     const data = await res.json();
     document.cookie = `token=${data};`;
     document.getElementById('signIn').classList.add('d-none');
+    document.getElementById('logOutBtn').disabled = false;
     showInfo();
 }
 
@@ -97,6 +114,10 @@ async function showInfo() {
                         login\
                         attrs\
                         auditRatio\
+                        skills: transactions(order_by: [{type: desc}, {amount: desc}] distinct_on: [type] where: {type: {_like: "skill_%"}}) {\
+                            type\
+                            amount\
+                        }\
                         audits {\
                             group {\
                                 captainId\
@@ -134,6 +155,9 @@ async function showInfo() {
     const event = Object.fromEntries(
         data.data.event.map((e) => [e.id, e.path])
     );
+    const skills = Object.fromEntries(
+        data.data.user[0].skills.map((s) => [s.type.slice(6), s.amount])
+    );
     const transaction = data.data.transaction;
     let total = 0;
     var dates = [];
@@ -142,13 +166,14 @@ async function showInfo() {
         .split(', ')[0];
     transaction.reverse().forEach((t) => {
         const date = new Date(t.createdAt).toLocaleString().split(', ')[0];
-        if (date !== prevDate) {
+        if (date !== prevDate && !t.path.includes('piscine-go')) {
             dates.push(prevDate);
             prevDate = date;
         }
     });
     const xp = {};
     transaction.forEach((t) => {
+        if (t.path.includes('piscine-go')) return;
         const date = new Date(t.createdAt).toLocaleString().split(', ')[0];
         xp[date] = Math.floor(t.amount + total);
         total += t.amount;
@@ -159,7 +184,7 @@ async function showInfo() {
     document.getElementById('contentEmail').innerHTML = user.attrs.email;
     document.getElementById('contentFName').innerHTML = user.attrs.firstName;
     document.getElementById('contentLName').innerHTML = user.attrs.lastName;
-    console.log(xp);
+    console.log(skills);
     const xpGraph = new Chart('xpGraph', {
         type: 'line',
         data: {
@@ -170,7 +195,7 @@ async function showInfo() {
                     data: Array.from(Object.values(xp)),
                     borderColor: 'rgb(255, 99, 132)',
                     backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                    tension: 0.5,
+                    tension: 0.1,
                 },
             ],
         },
@@ -182,6 +207,53 @@ async function showInfo() {
             },
         },
     });
+    const skillNames = {
+        html: 'HTML',
+        css: 'CSS',
+        js: 'JS',
+        sql: 'SQL',
+        go: 'Go',
+        unix: 'Unix',
+        tcp: 'TCP/IP',
+        'sys-admin': 'SysAdmin',
+        'front-end': 'Front-End',
+        'back-end': 'Back-End',
+        docker: 'Docker',
+        algo: 'Algorithms',
+        prog: 'Elementary Programming',
+    };
+    for (const [key, value] of Object.entries(skills)) {
+        const skill = document.createElement('div');
+        skill.classList.add('skill', 'col-2', 'm-1');
+        const heading = document.createElement('h3');
+        heading.innerHTML = skillNames[key] ? skillNames[key] : key;
+        const skillGraph = document.createElement('canvas');
+        skillGraph.id = key;
+        document.getElementById('skills').appendChild(skill);
+        skill.appendChild(heading);
+        skill.appendChild(skillGraph);
+        const graph = new Chart(key, {
+            type: 'doughnut',
+            data: {
+                labels: ['XP', 'Total'],
+                datasets: [
+                    {
+                        label: key,
+                        data: [value, 100 - value],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.5)',
+                            'rgba(255, 99, 132, 0.06)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(255, 99, 132, 0.5)',
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+        });
+    }
 }
 
 /**
